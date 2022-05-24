0b438e483fb055054f7268f20d92a3ac05422c25
shin8u <98867521+shin8u@users.noreply.github.com>
2022-05-24 03:15:26 +0300

    Update 11.pl

--- 11.pl
+++ 11.pl
@@ -44,3 +44,33 @@ pDown(K,Y,X1,Y1):-
 	X2 is X1 div 10,
 	pDown(K,Y,X2,Z).
 pDown(_,X,_,Y):- X is Y.
+%17
+
+kol_di(0,0):- ! .
+
+kol_di(X,Y): - X mod 10>3,
+          D is (X mod 10), D mod 2 >0,
+           X1 - X div 10, kol_di(X1,Y1), Y - Y1+1,!.
+kol_di(X,Y): - X1 is  X div 10, kol_di(X1,Y1), Y is  Y1.
+
+%18 
+
+kol_di_d(X,Y):- kol_di_d(X,Y,0),!.
+kol_di_d(X,Y,ACC):- X>0, X mod 10>3,
+          D is (X mod 10), D mod 2 >0,
+          X1 - X div 10, ACC1 - ACC+1, kol_di_d(X1,Y,ACC1).
+kol_di_d(X,Y,ACC):- X>0, X1 is X div 10, kol_di_d(X1,Y,ACC).
+kol_di_d(_,X,Y): -X is Y.
+
+
+%19
+
+fib(1,1):-!.
+fib(2,1):-!.
+fib(N,X):- N1 is N-1,N2 is N-2, fib(N1,X1),fib(N2,X2), X is X1+X2.
+
+%20 
+
+fib_d(N,X):- fib_d(N,X,1,1,2),!.
+fib_d(N,F,F,_,N):- !.
+fib_d(N,X,A,B,C):- A1 is A+B, C1 is C+1,fib_d(N,X,A1,A,C1).
